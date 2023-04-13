85ec86cdf966c879665342fd4fd96928dfe34043
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2023-04-13 11:39:57 -0300

    <refactor>função única para gerenciar as tarefas

--- js/tasks.js
+++ js/tasks.js
@@ -66,7 +66,9 @@ function createNewTaskEl(taskName, taskId) {
   todoIcon.classList.add("ph-duotone");
   todoIcon.classList.add("ph-circle-dashed");
   todoIcon.classList.add("check_btn");
-  todoIcon.addEventListener("click", completeTask);
+  todoIcon.addEventListener("click", (event) => {
+    toggleTaskCompletion(event, true);
+  });
 
   // done icon
   let doneIcon = document.createElement("i");
@@ -74,7 +76,9 @@ function createNewTaskEl(taskName, taskId) {
   doneIcon.classList.add("ph-check-circle");
   doneIcon.classList.add("check_btn");
   doneIcon.classList.add("hidden");
-  doneIcon.addEventListener("click", incompleteTask);
+  doneIcon.addEventListener("click", (event) => {
+    toggleTaskCompletion(event, false);
+  });
 
   // task name / p
   let name = document.createElement("p");
@@ -131,32 +135,62 @@ function addTask(event) {
   addTaskInput.value = "";
 }
 
-// complete task
-function completeTask(event) {
-  const todoIcon = event.target;
-  todoIcon.classList.add("hidden");
+function toggleTaskCompletion(event, isDone) {
+  const icon = event.target;
+  const task = icon.parentNode.parentNode;
 
-  const riskedText = todoIcon.parentNode.childNodes[2];
-  riskedText.classList.add("risked");
+  if (isDone) {
+    icon.classList.add("hidden");
+    task.classList.add("done");
+    task.classList.remove("todo");
+    task.querySelector(".ph-circle-dashed").classList.add("hidden");
+    task.querySelector(".ph-check-circle").classList.remove("hidden");
+    task.querySelector("p").classList.add("risked");
+  } else {
+    icon.classList.add("hidden");
+    task.classList.add("todo");
+    task.classList.remove("done");
+    task.querySelector(".ph-check-circle").classList.add("hidden");
+    task.querySelector(".ph-circle-dashed").classList.remove("hidden");
+    task.querySelector("p").classList.remove("risked");
+  }
 
-  const taskToCompleteId = todoIcon.parentNode.parentNode.id;
-  const taskToComplete = document.getElementById(taskToCompleteId);
+  const taskId = task.getAttribute("id");
+  const taskIndex = taskData.findIndex((task) => task.id === taskId);
 
-  taskToComplete.classList.add("done");
-  taskToComplete.classList.remove("todo");
-
-  const doneIcon = todoIcon.parentNode.childNodes[1];
-  doneIcon.classList.remove("hidden");
-
-  taskData.find((item) => {
-    if (item.id == taskToCompleteId) {
-      item.toDo = false;
-    }
-  });
-
-  setLocalStorage(taskData);
-  counter();
+  if (taskIndex !== -1) {
+    taskData[taskIndex].toDo = !isDone;
+    setLocalStorage(taskData);
+    counter();
+  }
 }
+
+// // complete task
+// function completeTask(event) {
+//   const todoIcon = event.target;
+//   todoIcon.classList.add("hidden");
+
+//   const riskedText = todoIcon.parentNode.childNodes[2];
+//   riskedText.classList.add("risked");
+
+//   const taskToCompleteId = todoIcon.parentNode.parentNode.id;
+//   const taskToComplete = document.getElementById(taskToCompleteId);
+
+//   taskToComplete.classList.add("done");
+//   taskToComplete.classList.remove("todo");
+
+//   const doneIcon = todoIcon.parentNode.childNodes[1];
+//   doneIcon.classList.remove("hidden");
+
+//   taskData.find((item) => {
+//     if (item.id == taskToCompleteId) {
+//       item.toDo = false;
+//     }
+//   });
+
+//   setLocalStorage(taskData);
+//   counter();
+// }
 
 // render task list to status recovery of tasks
 function renderTaskList() {
@@ -180,32 +214,32 @@ window.addEventListener("load", () => {
   renderTaskList();
 });
 
-// incomplete task
-function incompleteTask(event) {
-  const doneIcon = event.target;
-  doneIcon.classList.add("hidden");
+// // incomplete task
+// function incompleteTask(event) {
+//   const doneIcon = event.target;
+//   doneIcon.classList.add("hidden");
 
-  const riskedText = doneIcon.parentNode.childNodes[2];
-  riskedText.classList.add("risked");
+//   const riskedText = doneIcon.parentNode.childNodes[2];
+//   riskedText.classList.add("risked");
 
-  const taskToIncompleteId = doneIcon.parentNode.parentNode.id;
-  const taskToIncomplete = document.getElementById(taskToIncompleteId);
+//   const taskToIncompleteId = doneIcon.parentNode.parentNode.id;
+//   const taskToIncomplete = document.getElementById(taskToIncompleteId);
 
-  taskToIncomplete.classList.add("todo");
-  taskToIncomplete.classList.remove("done");
+//   taskToIncomplete.classList.add("todo");
+//   taskToIncomplete.classList.remove("done");
 
-  const todoIcon = doneIcon.parentNode.childNodes[0];
-  todoIcon.classList.remove("hidden");
+//   const todoIcon = doneIcon.parentNode.childNodes[0];
+//   todoIcon.classList.remove("hidden");
 
-  taskData.find((item) => {
-    if (item.id == taskToIncompleteId) {
-      item.toDo = true;
-    }
-  });
+//   taskData.find((item) => {
+//     if (item.id == taskToIncompleteId) {
+//       item.toDo = true;
+//     }
+//   });
 
-  setLocalStorage(taskData);
-  counter();
-}
+//   setLocalStorage(taskData);
+//   counter();
+// }
 
 // delete task
 function deleteTask(event) {
